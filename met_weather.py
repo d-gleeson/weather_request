5c5c8fc8ed04c21b4b23d8c52a189ed07551f5a6
Darragh <darraghgleeson1@gmail.com>
2016-02-22 22:02:35 +0000

    Initial commit of weather data request script

---
+++ met_weather.py
@@ -0,0 +1,24 @@
+import datetime
+import json
+import os
+import pandas as pd
+import requests
+
+root = os.environ['HOME']
+folder = os.path.join(root,'projects','weatherdata')
+
+
+date = datetime.datetime.now().strftime('%Y-%m-%d')
+file_name = date + '_allsites.json'
+file_path = os.path.join(folder,'json_files',file_name)
+
+with open('key.txt', 'r') as f:
+	key = json.load(f)['key']
+
+url = 'http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/all?res=hourly&key='
+
+req = requests.get(url+key)
+
+with open(file_path, 'wb') as f:
+	for chunk in req.iter_content(100000):
+		f.write(chunk)
