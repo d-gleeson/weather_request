36494280428de405416035aef9da7d895220d73f
Darragh <darraghgleeson1@gmail.com>
2016-02-23 20:35:45 +0000

    fixed absolute path issue for keys file

--- met_weather.py
+++ met_weather.py
@@ -6,13 +6,14 @@ import requests
 
 root = os.environ['HOME']
 folder = os.path.join(root,'projects','weatherdata')
+key_path = os.path.join(folder, 'key.txt')
 
 
 date = datetime.datetime.now().strftime('%Y-%m-%d')
 file_name = date + '_allsites.json'
 file_path = os.path.join(folder,'json_files',file_name)
 
-with open('key.txt', 'r') as f:
+with open(key_path, 'r') as f:
 	key = json.load(f)['key']
 
 url = 'http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/all?res=hourly&key='
